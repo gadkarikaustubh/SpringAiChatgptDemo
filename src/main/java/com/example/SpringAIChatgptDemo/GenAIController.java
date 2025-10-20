316242be19753b28a9149d3f9d9de512a2f64ade
kaustubh <kaustubhgadkari2@gmail.com>
2025-10-20 11:39:00 +0530

    Commt first time

---
+++ src/main/java/com/example/SpringAIChatgptDemo/GenAIController.java
@@ -0,0 +1,22 @@
+package com.example.SpringAIChatgptDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenAIController {
+
+	
+	private final ChatService chatService;
+
+	public GenAIController(ChatService chatService) {
+		this.chatService = chatService;
+	}
+	
+	@GetMapping("ask-ai")
+	public String getAiData(@RequestParam String prompt) {
+		return chatService.getResponse(prompt);
+	}
+	
+}
